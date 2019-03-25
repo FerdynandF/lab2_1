0ee320cc94ed9cd1f73c5b26ce77afaf5540fd38
felsztukier.ferdynand <209306@edu.p.lodz.pl>
2019-03-25 17:15:02 +0100

    testy_v1 test checking whether an element is on the first position added; small changes in search() -> position index should start from 0.

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -5,19 +5,25 @@ import org.junit.Test;
 
 public class BinarySearchTest {
 
-    public static final int ELEMENT = 3;
-    public static final int ANOTHER_ELEMENT = 5;
+    private static final int ELEMENT = 3;
+    private static final int ANOTHER_ELEMENT = 5;
 
     @Test
-    public void elementInSingleSequence(){
+    public void elementInSingleSequence() {
         int[] sequence = {ELEMENT};
         Assert.assertTrue(BinarySearch.search(ELEMENT, sequence).isFound());
     }
 
     @Test
-    public void elementNotInSingleSequence(){
+    public void elementNotInSingleSequence() {
         int[] sequence = {ELEMENT};
         Assert.assertFalse(BinarySearch.search(ANOTHER_ELEMENT, sequence).isFound());
+    }
+
+    @Test
+    public void elementOnFirstPositionSequence() {
+        int[] sequence = {ELEMENT, ANOTHER_ELEMENT};
+        Assert.assertEquals(0,BinarySearch.search(ELEMENT, sequence).getPosition());
     }
 
 }
